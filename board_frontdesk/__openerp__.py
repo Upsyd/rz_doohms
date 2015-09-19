c59d84393f6ef88e96e624a4ba35af3993bb61e1
Riza Kurniawan <rizabisnis@gmail.com>
2015-09-19 20:21:20 +0700

    board front desk/reception

---
+++ board_frontdesk/__openerp__.py
@@ -0,0 +1,26 @@
+
+{
+    "name": "Board for Hotel FrontDesk",
+    "version": "1.0",
+    "author": "Riza and Team",
+    "website": "rizaontheblog.blogspot.com",
+    "category": "Board/Hotel FrontDesk",
+    "depends": [
+        "board",
+        "report_hotel_restaurant",
+        "hotel_pos_restaurant"
+        ],
+    "data": [
+        "views/board_frontdesk_view.xml"
+    ],
+    "description": """
+This module implements a dashboard for hotel FrontDesk that includes:
+    * Calendar view of Today's Check-In and Check-Out
+    * Calendar view of Weekly Check-In and Check-Out
+    * Calendar view of Monthly Check-In and Check-Out
+    """,
+    "active": False,
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
