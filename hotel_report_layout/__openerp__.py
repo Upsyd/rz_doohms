ba7b3d14c149b1be937c26956cf0b0211c08e889
Riza Kurniawan <rizabisnis@gmail.com>
2015-09-19 20:16:08 +0700

    hotel report

---
+++ hotel_report_layout/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Report Extended',
+    'category': 'Base',
+    'summary': 'Report',
+    'version': '0.01',
+    'description': """
+        Report Extended Which Insert The Header Of Company's Full Address
+        In Qweb Report
+            """,
+    "author": "Riza and Team",
+    "website": "rizaontheblog.blogspot.com",
+    'depends': ['report'],
+    'data': [
+        'views/layouts.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
